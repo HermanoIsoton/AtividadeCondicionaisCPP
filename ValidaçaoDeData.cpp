2248f8d65dc45846aea61a6378e5390072bad222
HermanoIsoton <hermanoisoton023@gmail.com>
2026-03-31 22:54:30 -0300

    atividade

--- ValidaçaoDeData.cpp
+++ ValidaçaoDeData.cpp
@@ -6,7 +6,7 @@ int main(){
 
     cout << "Digite o |dia| o |mes| e o |ano| do seu nascimento" << endl;
     cin >> dia >> mes >> ano;
-    // validação do mes, dia, ano
+    // validação do mes, dia e ano
     bool d = ((mes==3|| mes==5|| mes==7||mes==8||mes==10||mes==12) && (dia<=31) || (mes==1||mes==4||mes==6||mes==9||mes==11) && (dia<=30) || (mes==2) && (dia<=28));
     bool m = (mes >= 1 && mes <= 12);
     bool a = (ano >= 1926 || ano <= 2026 );
@@ -14,7 +14,7 @@ int main(){
     //validação de data
     if ((m) && (a) && (d)) cout << "data valida";
     else cout << "data invalida | ";
-    
+
     // razão
     if(!m) cout << "mes invalido | ";
     if(!d) cout << "dia invalido | ";
